5151680e8e448726bd06022f92b401568a3e1f32
imishab <mishabmsb91@gmail.com>
2026-02-06 10:08:21 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
-export default nextConfig;
+const nextConfig = {};
+
+module.exports = withPWA(nextConfig);
